7194d507ca1614a0b76802b92ee38c34e9de3c1d
José Henrique Firmino da Silva <zhenrique.piox@gmail.com>
2018-02-21 14:28:35 -0300

    Testando o envio ao git

---
+++ app/src/main/java/br/gov/pb/tce/despesalegalbeta/LoginActivity.java
@@ -0,0 +1,66 @@
+package br.gov.pb.tce.despesalegalbeta;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+
+    private Button botaoLogin;
+    private EditText campoEmail;
+    private EditText campoSenha;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        }else{
+
+        }
+
+        campoEmail = (EditText) findViewById(R.id.emailID);
+        campoSenha = (EditText) findViewById(R.id.senhaID);
+        botaoLogin = (Button) findViewById(R.id.btLoginID);
+
+        botaoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signInWithEmailAndPassword(campoEmail.getText().toString(),campoSenha.getText().toString()).
+                        addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()){
+                                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                                    Log.i("Signin","TESTE OK");
+                                }else{
+                                    Log.i("Signin","TESTE ERRO");
+                                    Toast.makeText(LoginActivity.this,"ERRO AO AUTENTICAR",Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/br/gov/pb/tce/despesalegalbeta/MainActivity.java
@@ -0,0 +1,26 @@
+package br.gov.pb.tce.despesalegalbeta;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent());
+        }else{
+
+        }
+    }
+}
