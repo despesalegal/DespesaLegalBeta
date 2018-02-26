ce847e21f10bdd2fe077839a08b1ff330fe64c30
José Henrique Firmino da Silva <zhenrique.piox@gmail.com>
2018-02-26 09:53:54 -0300

    Capturando localização

--- app/src/main/java/br/gov/pb/tce/despesalegalbeta/MainActivity.java
+++ app/src/main/java/br/gov/pb/tce/despesalegalbeta/MainActivity.java
@@ -1,22 +1,37 @@
 package br.gov.pb.tce.despesalegalbeta;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks, LocationListener {
 
     //cria variaveis
     private FirebaseAuth firebaseAuth;
@@ -24,21 +39,34 @@ public class MainActivity extends AppCompatActivity {
     private ImageView botaoSair, photo;
     private Button botaoCamera, btPhoto;
 
+    //variaveis de localização
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    private TextView localizacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //cria a api do google
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
         //instancia autenticador
         firebaseAuth = FirebaseAuth.getInstance();
         photo = (ImageView) findViewById(R.id.photo);
-        botaoSair= (ImageView) findViewById(R.id.btSairID);
+        botaoSair = (ImageView) findViewById(R.id.btSairID);
         botaoCamera = (Button) findViewById(R.id.btCameraID);
+        localizacao = (TextView) findViewById(R.id.locationID);
 
         botaoSair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,LoginActivity.class));       //volta pra tela de login
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));       //volta pra tela de login
                 firebaseAuth.signOut();                                                             //desvalida a sessao
             }
         });
@@ -52,19 +80,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
+    }
+
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            SaveImage (imageBitmap);
+            SaveImage(imageBitmap);
         }
     }
 
@@ -78,17 +118,83 @@ public class MainActivity extends AppCompatActivity {
         n = generator.nextInt();
         String fname = "Image-" + n + ".jpg";
         File file = new File(myDir, fname);
-        if(file.exists()) file.delete();
-        try{
+        if (file.exists()) file.delete();
+        try {
             FileOutputStream out = new FileOutputStream(file);
             imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
             out.flush();
             out.close();
-        } catch (Throwable e){
+        } catch (Throwable e) {
             e.printStackTrace();
         }
-        return  file.getAbsolutePath();
+        return file.getAbsolutePath();
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+        localizacao.setText("Atualização de localização " + location.toString());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(1000);
+
+        //bloco recomendado pelo android studio
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //captura ultima localização conhecida
+
+        Location ultimaLocalizacao = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+        //captura dados de localização
+        double latitude = ultimaLocalizacao.getLatitude();
+        double longitude = ultimaLocalizacao.getLongitude();
+        double altitude = ultimaLocalizacao.getAltitude();
+
+        //Formata data e hora
+        DateFormat formatacaoData = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date(ultimaLocalizacao.getTime());
+        String dataFormatada = formatacaoData.format(date);
+
+        //testes em uma textview
+        //localizacao.setText(dataFormatada);
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
